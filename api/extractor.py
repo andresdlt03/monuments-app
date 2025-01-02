793f3eaff642141bed36427c166453f2090670f5
andresdlt03 <andelatmo@gmail.com>
2025-01-02 19:40:40 +0100

    added extractor endpoints tags

--- api/extractor.py
+++ api/extractor.py
@@ -2,14 +2,14 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.post("/mur")
+@router.post("/mur", tags=["extractor"])
 async def extract_mur():
     return {"message": "MUR extracted"}
 
-@router.post("/cv")
+@router.post("/cv", tags=["extractor"])
 async def extract_cv():
     return {"message": "CV extracted"}
 
-@router.post("/cat")
+@router.post("/cat", tags=["extractor"])
 async def extract_cat():
     return {"message": "CAT extracted"}
